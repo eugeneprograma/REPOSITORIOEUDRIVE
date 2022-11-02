3fe2e1178c270236812b10dd3ec4ca0c473ba3d7
eugeneprograma <ingenierofinancieropuj@gmail.com>
2022-11-01 21:28:41 -0500

    Create Digiturno.js

---
+++ HTML/SEMANA 15/js/Digiturno.js
@@ -0,0 +1,33 @@
+function prefer(){
+    let num=1;
+    let res=true;
+        while (num<=99 &&  res==true){
+        numUser= prompt("Ingrese su numero de usuario");
+        if (nomuser!==null){
+            var n=num.toString().padStart(2,'0');
+            //toString: poner numero a texto
+            //padStart: para rellenar dos digitos digitos a la izquierda. En este caso coloca DOS digitos, se pueden colocar mas.
+            res=confirm("ESTIMADO USUARIO:"+" "+" "+nomuser+"\n"+"SU TURNO ES:"+" "+"E"+n+"\n"+"ESPERE SU LLAMADO...");
+            num++;
+            }else{
+                alert("ERROR EN TURNO …");
+                break;
+            }
+        }
+    }
+    function user(){
+        let num=1;
+        let res=true;
+        while (num<=99 && res==true){
+            nomuser=prompt("INGRESE SU NOMBRE DE USUARIO");
+            if (nomuser!==null){
+                var n=num.toString().padStart(2,'0');
+                res=confirm("ESTIMADO USUARIO:"+" "+" "+nomuser+"\n"+"SU TURNO ES:"+" "+"A"+n+"\n"+"ESPERE SU LLAMADO...");
+                num++;
+                }else{
+                    alert("ERROR EN TURNO…");
+                    break;
+                }
+            }
+        }
+
