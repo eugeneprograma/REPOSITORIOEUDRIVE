7858167f3fab5dfc69f11f546123e5ffb307eec5
eugeneprograma <ingenierofinancieropuj@gmail.com>
2022-11-07 13:32:57 -0500

    Update Apostar.js

--- HTML/SEMANA 15/js/Apostar.js
+++ HTML/SEMANA 15/js/Apostar.js
@@ -2,7 +2,7 @@ function apostar(){
     var a=Math.round(Math.random()*10);
     //round. aproxima numero hacia arriba. Ej: si saale 4,6 lo aproxima a 5
     //random*10 , el 10 indica que los aleatorios van de 1 a 10, es decir se puede poner cualquier random. Ej: random()*50 > haria 50 numeros aleatorios
-    
+//getElementById: unir variables de 
     document.getElementById("resultado").value=a;
     var b=document.getElementById("apostado").value;
     if (a==b){
@@ -11,7 +11,7 @@ function apostar(){
       document.getElementById("salida").value="perdió";
     }
   }
-  function cancel(){
+  function cancel(){ //borrar contenido y dejar espacio blanco
     document.getElementById("apostado").value=" ";
     document.getElementById("resultado").value=" ";
     document.getElementById("salida").value=" ";
