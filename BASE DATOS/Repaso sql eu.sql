283441b2da3335ca6d595c718d1a73152dbc5a41
Eugenio Vera Gómez <110945257+eugeneprograma@users.noreply.github.com>
2022-12-13 23:18:05 -0500

    Update Repaso sql eu.sql

--- BASE DATOS/Repaso sql eu.sql
+++ BASE DATOS/Repaso sql eu.sql
@@ -7,6 +7,12 @@ tipo_animal varchar(300) default null,
 estado_animo varchar(200) default null,
 primary key (`id_animal`)
 );
+insert into animales (tipo_animal,estado_animo) values ('dinosaurio','feroz');
+insert into animales (tipo_animal,estado_animo) values ('perro','feroz');
+insert into animales (tipo_animal,estado_animo) values ('gato','triste');
+insert into animales (tipo_animal,estado_animo) values ('pollito','feliz');
+-- VISUALIZAR LA TABLA
+select * from animales;
 
 
 
