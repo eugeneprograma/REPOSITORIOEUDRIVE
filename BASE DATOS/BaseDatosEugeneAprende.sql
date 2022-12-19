f27b4172481282b034e8cb1086df030aeeedcdf0
eugeneprograma <ingenierofinancieropuj@gmail.com>
2022-12-19 18:02:41 -0500

    Update BaseDatosEugeneAprende.sql

--- BASE DATOS/BaseDatosEugeneAprende.sql
+++ BASE DATOS/BaseDatosEugeneAprende.sql
@@ -10,6 +10,25 @@ estado varchar (200) default null,
 primary key (id_animal)
 );
 
--- ingresar valores a tabla:
+-- insertar valores a la tabla:
+insert into animales (nombre_animal, estado) values ('perro','en proteccion');
+insert into animales (nombre_animal, estado) values ('aguila', 'libre');
+insert into animales (nombre_animal, estado) values ('conejo', 'libre');
+insert into animales (nombre_animal, estado) values ('pollito', 'en proteccion');
+insert into animales (nombre_animal, estado) values ('canguro', 'en proteccion');
+-- VISUALIZAR TABLA
+select * from animales;
+-- HACER UNA CONSULTA
+select * from animales where id_animal =2;
+select * from animales where estado="en proteccion";
+-- CONSULTA CON MAS CONDICIONES "Y"
+select * from animales where nombre_animal="perro" and estado="en proteccion";
+
+select * from animales where estado="tonto";
+
+
+
+
+
 
 
