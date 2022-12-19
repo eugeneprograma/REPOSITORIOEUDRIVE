f85dac8ac88c5c222291dc0475da8a1f885777a4
eugeneprograma <ingenierofinancieropuj@gmail.com>
2022-12-19 17:40:21 -0500

    Update Digiturno.js

--- HTML/SEMANA 15/js/Digiturno.js
+++ HTML/SEMANA 15/js/Digiturno.js
@@ -2,11 +2,11 @@ function prefer(){
     let num=1;
     let res=true;
         while (num<=99 &&  res==true){
-        numUser= prompt("Ingrese su numero de usuario");
+        numUser= prompt("Ingrese su numero de usuario PREFERENCIAL");
         if (nomuser!==null){
             var n=num.toString().padStart(2,'0');
             //toString: poner numero a texto
-            //padStart: para rellenar dos digitos digitos a la izquierda. En este caso coloca DOS digitos, se pueden colocar mas.
+            //padStart: para rellenar dos digitos a la izquierda. En este caso coloca DOS digitos, se pueden colocar mas.
             res=confirm("ESTIMADO USUARIO:"+" "+" "+nomuser+"\n"+"SU TURNO ES:"+" "+"E"+n+"\n"+"ESPERE SU LLAMADO...");
             num++;
             }else{
@@ -15,11 +15,12 @@ function prefer(){
             }
         }
     }
-    function user(){
+
+function user(){
         let num=1;
         let res=true;
         while (num<=99 && res==true){
-            nomuser=prompt("INGRESE SU NOMBRE DE USUARIO");
+            nomuser=prompt("INGRESE SU NOMBRE DE USUARIO NO PREFERENCIAL");
             if (nomuser!==null){
                 var n=num.toString().padStart(2,'0');
                 res=confirm("ESTIMADO USUARIO:"+" "+" "+nomuser+"\n"+"SU TURNO ES:"+" "+"A"+n+"\n"+"ESPERE SU LLAMADO...");
