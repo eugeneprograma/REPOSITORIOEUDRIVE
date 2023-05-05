cd9571cdc156fc0722c1799c38d8681ed3a35d5b
Eugenio Vera Gómez <ingenierofinancieropuj@gmail.com>
2023-05-05 17:29:02 -0500

    Create repaso clase 4 mayo.py

---
+++ PYTHON/repaso clase 4 mayo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  5 17:15:20 2023
+
+@author: eugeniovera
+"""
+
+#repaso de diccionarios
+selecciones = {
+    "jugador":["luis","pedro","fabian","camilo","carlos"],
+    "equipo":["junior","millonarios","sta","cali","sporning"]
+        
+    }
