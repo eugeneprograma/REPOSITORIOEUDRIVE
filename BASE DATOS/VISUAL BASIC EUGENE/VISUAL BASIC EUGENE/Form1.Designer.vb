e777e31c5ec6daef63ebbd1e1753fe694a1a3a84
Eugenio Vera Gómez <ingenierofinancieropuj@gmail.com>
2023-05-05 16:47:08 -0500

    Create Form1.Designer.vb

---
+++ BASE DATOS/VISUAL BASIC EUGENE/VISUAL BASIC EUGENE/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Formulario
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        ListBox1 = New ListBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(84, 64)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(94, 32)
+        Label1.TabIndex = 0
+        Label1.Text = "Meses :"
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 32
+        ListBox1.Location = New Point(228, 74)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(241, 36)
+        ListBox1.TabIndex = 1
+        ' 
+        ' Formulario
+        ' 
+        AutoScaleDimensions = New SizeF(13F, 32F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(ListBox1)
+        Controls.Add(Label1)
+        Name = "Formulario"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ListBox1 As ListBox
+End Class
