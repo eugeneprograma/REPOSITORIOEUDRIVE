58557fe06beb51a2453f1dd2def6510754ee437d
Eugenio Vera Gómez <ingenierofinancieropuj@gmail.com>
2023-05-30 08:51:29 -0500

    Create clase 18 mayo.py

---
+++ PYTHON/clase 18 mayo.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 18 18:48:07 2023
+
+@author: eugeniovera
+"""
+
+from flask import Flask
+sw = Flask(__name__)
+
+@sw.route('/')
+def inicio():
+    return "pagina inicial ..."
+
+@sw.route('/listado')
+def lista_elemementos():
+    return 'listado de productos'
+
+if __name__=='__main__':
+    
+    sw.run(debug=True, port=8000)
